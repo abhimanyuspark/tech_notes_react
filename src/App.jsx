64bd65285ceb83dda14d12ab46b646851a03d999
abhimanyuspark <127104226+abhimanyuspark@users.noreply.github.com>
2025-02-25 22:02:42 +0530

    persist

--- src/App.jsx
+++ src/App.jsx
@@ -25,23 +25,25 @@ function App() {
             <Route path="login" element={<Login />} />
             <Route path="*" element={<NotFound />} />
 
-            <Route element={<Persist />}>
-              <Route path="dash" element={<DashLayout />}>
-                <Route index element={<Welcome />} />
+            {/* <Route element={<Persist />}> */}
+            {/* Dash start */}
+            <Route path="dash" element={<DashLayout />}>
+              <Route index element={<Welcome />} />
 
-                <Route path="users">
-                  <Route index element={<UsersList />} />
-                  <Route path="new" element={<UserForm />} />
-                  <Route path="edit/:id" element={<UserForm />} />
-                </Route>
+              <Route path="users">
+                <Route index element={<UsersList />} />
+                <Route path="new" element={<UserForm />} />
+                <Route path="edit/:id" element={<UserForm />} />
+              </Route>
 
-                <Route path="notes">
-                  <Route index element={<NotesList />} />
-                  <Route path="new" element={<NoteForm />} />
-                  <Route path="edit/:id" element={<NoteForm />} />
-                </Route>
+              <Route path="notes">
+                <Route index element={<NotesList />} />
+                <Route path="new" element={<NoteForm />} />
+                <Route path="edit/:id" element={<NoteForm />} />
               </Route>
             </Route>
+            {/* Dash end */}
+            {/* </Route> */}
           </Route>
         </Routes>
       </Suspense>
